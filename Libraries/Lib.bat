e3d779d6ed720f8204bd74a75e9dc2558150c84f
Mordojo <mordojo09@gmail.com>
2020-04-07 18:42:43 -0400

    Added Libraries file

---
+++ Libraries/Lib.bat
@@ -0,0 +1,5 @@
+pip install robotframework
+pip install --upgrade robotframework-seleniumlibrary
+pip install --upgrade RESTinstance
+pip install TestLink-API-Python-client
+pip install allure-robotframework
